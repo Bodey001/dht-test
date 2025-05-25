5f16577efda449d93f7f38ecf3c6ac2af5eab073
Olabode <oyewunmi1010@gmail.com>
2025-05-25 12:35:09 +0100

    Refactored the code

---
+++ README.md
@@ -0,0 +1,13 @@
+Setup:
+
+Clone the repository
+`git clone https://github.com/Bodey001/dht-test`
+
+Download the extensions
+C/C++
+Wokwi Simulator
+PlatformIO IDE
+
+Build the ./src/main.cpp file
+
+Run the simulation in diagram.json file
--- src/main.cpp
+++ src/main.cpp
@@ -2,95 +2,93 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <LiquidCrystal_I2C.h>
 
-// Define the GPIO pin connected to the DHT22 data pin
+// Define the GPIO pin and their connections
 #define DHTPIN 23
 #define DHTTYPE DHT22
-
-// Define
+#define relayModule 12
+#define I2C_SDA 21
+#define I2C_SCL 22
 
 // Create an object of the DHTesp class
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
-uint32_t delayMS;
+// set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   // Initialize device.
   dht.begin();
-  Serial.println(F("DHTxx Unified Sensor Example"));
+  Serial.println(F("DHT22 Sensor Test"));
+  pinMode(relayModule, OUTPUT);
+  Serial.println("LED Control Example");
+  // initialize the lcd to use user defined I2C pins
+  lcd.init(I2C_SDA, I2C_SCL);
+  lcd.backlight();
+  lcd.print("Initializing...");
   // Print temperature sensor details.
   sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
-  Serial.println(F("------------------------------------"));
-  Serial.println(F("Temperature Sensor"));
-  Serial.print(F("Sensor Type: "));
-  Serial.println(sensor.name);
-  Serial.print(F("Driver Ver:  "));
-  Serial.println(sensor.version);
-  Serial.print(F("Unique ID:   "));
-  Serial.println(sensor.sensor_id);
-  Serial.print(F("Max Value:   "));
-  Serial.print(sensor.max_value);
-  Serial.println(F("°C"));
-  Serial.print(F("Min Value:   "));
-  Serial.print(sensor.min_value);
-  Serial.println(F("°C"));
-  Serial.print(F("Resolution:  "));
-  Serial.print(sensor.resolution);
-  Serial.println(F("°C"));
-  Serial.println(F("------------------------------------"));
-  // Print humidity sensor details.
-  dht.humidity().getSensor(&sensor);
-  Serial.println(F("Humidity Sensor"));
-  Serial.print(F("Sensor Type: "));
-  Serial.println(sensor.name);
-  Serial.print(F("Driver Ver:  "));
-  Serial.println(sensor.version);
-  Serial.print(F("Unique ID:   "));
-  Serial.println(sensor.sensor_id);
-  Serial.print(F("Max Value:   "));
-  Serial.print(sensor.max_value);
-  Serial.println(F("%"));
-  Serial.print(F("Min Value:   "));
-  Serial.print(sensor.min_value);
-  Serial.println(F("%"));
-  Serial.print(F("Resolution:  "));
-  Serial.print(sensor.resolution);
-  Serial.println(F("%"));
-  Serial.println(F("------------------------------------"));
-  // Set delay between sensor readings based on sensor details.
-  delayMS = sensor.min_delay / 1000;
+}
+
+void relayOutput(bool condition)
+{
+  if (condition)
+  {
+    digitalWrite(relayModule, HIGH);
+    Serial.println("Condition met, LED is ON");
+  }
+  else
+  {
+    digitalWrite(relayModule, LOW);
+    Serial.println("Condition not met, LED is OFF");
+  }
 }
 
 void loop()
 {
-  // Delay between measurements.
-  delay(delayMS);
-  // Get temperature event and print its value.
+  //Measurement
+  delay(2000);
+  // Get temperature and humidity event and print its value.
   sensors_event_t event;
+
   dht.temperature().getEvent(&event);
-  if (isnan(event.temperature))
+  float temperature = event.temperature;
+
+  dht.humidity().getEvent(&event);
+  float relative_humidity = event.relative_humidity;
+
+  if (isnan(temperature) || isnan(relative_humidity))
   {
-    Serial.println(F("Error reading temperature!"));
+    Serial.println(F("Error reading data!"));
   }
   else
   {
     Serial.print(F("Temperature: "));
-    Serial.print(event.temperature);
+    Serial.print(temperature);
     Serial.println(F("°C"));
-  }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity))
-  {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else
-  {
     Serial.print(F("Humidity: "));
-    Serial.print(event.relative_humidity);
+    Serial.print(relative_humidity);
     Serial.println(F("%"));
-  }
-}
+  };
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temperature);
+  lcd.print("°C");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Hum: ");
+  lcd.print(relative_humidity);
+  lcd.print("%");
+
+  bool tempCondition = temperature >= 24;
+  bool humidityCondition = relative_humidity <= 50;
+
+  bool condition = tempCondition || humidityCondition;
+
+  relayOutput(condition);
+};
